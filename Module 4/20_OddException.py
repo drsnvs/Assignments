594cbddb3dd7820df99e10d178b6d4ba093d8bc8
Darshan Vs <109671745+drsnvs@users.noreply.github.com>
2022-08-25 21:38:21 +0530

    Exceptions

--- Module 4/20_OddException.py
+++ Module 4/20_OddException.py
@@ -1,8 +1,11 @@
 # Write python program that user to enter only odd numbers, else will raise an exception.
 
-numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-try:
-    for i in numbers:
-        print(i==i*2)
-except Exception as ex:
-    print(ex)
+# numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+try: 
+    num = int(input("Enter Number: "))
+    if num%2==0:
+
+        raise NameError("Odd Only")  # Raise Error
+except NameError:
+    print ("An exception")
+    raise  # To determine whether the exception was raised or not
