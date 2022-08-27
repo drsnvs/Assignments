9a7be2126c40c067e0974bc40920c6cb5368ef0d
Darshan Vs <109671745+drsnvs@users.noreply.github.com>
2022-08-27 10:25:53 +0530

    Association

---
+++ Module 4/28_CourseFaculty.py
@@ -0,0 +1,5 @@
+# 28. What relationship is appropriate for Course and Faculty?
+
+# association relationship is appropriate for Course and Faculty
+
+# An association relationship can be represented as one-to-one, one-to-many, or many-to-many (also known as cardinality). Essentially, an association relationship between two or more objects denotes a path of communication (also called a link) between them so that one object can send a message to another.
---
+++ Module 4/29_StudentPerson.py
@@ -0,0 +1,5 @@
+# 29. What relationship is appropriate for Student and Person?
+
+# association relationship is appropriate for Student and Person
+
+# An association relationship can be represented as one-to-one, one-to-many, or many-to-many (also known as cardinality). Essentially, an association relationship between two or more objects denotes a path of communication (also called a link) between them so that one object can send a message to another.
