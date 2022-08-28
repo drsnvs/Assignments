1899e1cfac850c5167c653374c5a2299437faef3
Darshan Vs <109671745+drsnvs@users.noreply.github.com>
2022-08-28 10:18:48 +0530

    Freeze
    python freeze -> requirements.txt

---
+++ Module 5/Check_version_installed.py
@@ -0,0 +1,20 @@
+# 2. How to check installed version of django?
+
+# Create a project in Django
+
+# Fisrt of all create a virtual Enviorment
+# python -m venv Virtual_Enviourment_name
+
+# Second install django 
+# pip install django
+
+# Then create Project
+# django-admin startproject Project_name .
+
+# Then creat App
+# django-admin startapp App_Name
+
+# For check installed version you have to create one text file and store in it
+# pip freeze -> requerements.txt
+
+# After that you can see the installed version in requerements.txt file
