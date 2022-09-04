f8b33490a7714c623a31237c41873b0bd4df0131
Darshan Vs <109671745+drsnvs@users.noreply.github.com>
2022-09-04 17:58:43 +0530

    Connected with another class

---
+++ Assignment-3/3_Student.py
@@ -0,0 +1,9 @@
+import Student as DA
+
+da = DA.Student()
+da.set_id()
+da.set_name()
+da.set_marks()
+print("Your Id Is: ",da.get_id())
+print("Your Name Is:",da.get_name())
+print("Your Marks Is:",da.get_marks())  
